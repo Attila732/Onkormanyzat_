e3e6205da4ef66dc7a4556351b438f93276157c3
MrBrown16 <morabarna16@gmail.com>
2024-02-08 00:12:09 +0100

    404 not found is handled(will be handled) by angular routing module, /client/ prefix added to url for angular specific routes index.html is the fallback route

--- src/main/java/hu/project/groupproject/gatewayfrontend/config/routes/RouteConfig.java
+++ src/main/java/hu/project/groupproject/gatewayfrontend/config/routes/RouteConfig.java
@@ -4,69 +4,52 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 
 @Configuration
 public class RouteConfig {
 
-	// @Bean
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/").addResourceLocations("classpath:target\\classes\\static\\index.html");
-    }
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(
+						r -> r
+								.path("/issuer-uri/**")
+								.filters(
+										f -> f
+												.rewritePath("/issuer-uri/", "/")
+												.tokenRelay())
+								.uri("http://localhost:8083"))
+				.route(
+						r -> r
+								.path("/resource/**")
+								.filters(
+										f -> f
+												.rewritePath("/resource/", "/")
+												.tokenRelay())
+								.uri("http://localhost:8082"))
+				.route(
+						r -> r
+								.path("/login/**")
+								.filters(
+										f -> f
+												.tokenRelay())
+								.uri("http://localhost:8083"))
+				.route(
+						r -> r
+								.path("/user/**")
+								.filters(
+										f -> f
+												.tokenRelay())
+								.uri("http://localhost:8083"))
+				.route(r -> r
+						.path("/{path:^(?!.*\\.js$)(?!.*\\.css$)(?!.*\\.ico$)(?!.*\\.html$)(?!.*\\.woff2$)(?!.*\\.[^.]+\\.[0-9]+\\.[^.]+$).*}")
+						.uri("forward:/index.html"))
 
-    @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-			.route(
-				r -> r
-					.path("/resource/**")
-					.filters(
-						f -> f
-							.rewritePath("/resource/", "/")
-							.tokenRelay()
-					)
-					.uri("http://localhost:8082")
-			)
-			.route(
-				r -> r
-					.path("/user/**")
-					.filters(
-						f -> f
-							// .rewritePath("/resource/", "/")
-							.tokenRelay()
-					)
-					.uri("http://localhost:8083")
-		)
-		.route(
-			r -> r
-				.path("/issuer-uri/**")
-				.filters(
-					f ->f 
-						.rewritePath("/issuer-uri/", "/")	
-						.tokenRelay()
-				)
-				.uri("http://localhost:8083")
-		)
-		.route(
-			r -> r
-				.path("/login/**")
-				.filters(
-					f -> f
-						// .rewritePath("/login/", "/")
-						.tokenRelay()
-				)
-				.uri("http://localhost:8083")
-		)
-		// .route(
-		// 	r->r
-		// 	.path("/")
-		// 	.filters(
-		// 		f->f
-		// 		.tokenRelay()
-		// 	)
-		// 	.uri("http://localhost:8081/angular")
-		// )
-		//TODO: add /client/ for frontend path, and route everything to the index.html(create 404 page)
-		.build();
-    }
+				.route(
+						r -> r
+								.path("/client/**")
+								.uri("forward:/index.html"))
+
+				.build();
+	}
 }
