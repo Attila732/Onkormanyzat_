b37cace22b2f81e24a385ac6ad692a4cadb81c9e
MrBrown16 <morabarna16@gmail.com>
2024-02-28 10:07:08 +0100

    file upload started (not working)

--- Onkormanyzat_/src/app/barna-test-cuccosa.service.ts
+++ Onkormanyzat_/src/app/barna-test-cuccosa.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -10,39 +10,19 @@ export class BarnaTestCuccosaService {
 
   constructor(private http:HttpClient) { }
   postMakeAdmin(username:any){
-    this.http.post(this.urlUser+"make-admin",username).subscribe({
-      next:(res)=>{
-        console.log(res)
-        return res
-      },
-      error:(err)=>{
-        console.log("Error in post make-admin ")
-        console.log(err)
-      }
-    })
+    return this.http.post(this.urlUser+"make-admin",username)
+    
   }
   postHello(ize:any){
-    this.http.post(this.urlRes+"hello",ize).subscribe({
-      next:(res)=>{
-        console.log(res)
-        return res
-      },
-      error:(err)=>{
-        console.log("Error in post hello ")
-        console.log(err)
-      }
-    })
+    return this.http.post(this.urlRes+"hello",ize)
+    
+  }
+  postFile(file:any){
+    // var httpOptions={headers: new HttpHeaders({"Content-Type":"image/png"})}
+    return this.http.post(this.urlRes+"img",file)
+    
   }
   getHello(){
-    this.http.get(this.urlRes+"hello").subscribe({
-      next:(res)=>{
-        console.log(res)
-        return res
-      },
-      error:(err)=>{
-        console.log("Error in get ")
-        console.log(err)
-      }
-    })
+    return this.http.get(this.urlRes+"hello")
   }
 }
--- Onkormanyzat_/src/app/btccomp/btccomp.component.ts
+++ Onkormanyzat_/src/app/btccomp/btccomp.component.ts
@@ -7,9 +7,12 @@ import { BarnaTestCuccosaService } from '../barna-test-cuccosa.service';
   styleUrls: ['./btccomp.component.css']
 })
 export class BTCCompComponent {
-
+  makeAdmin:any
+  postHelloRes:any
+  getHelloRes:any
   username:any
   ize:{"ize":"ize"}={"ize":"ize"}
+  file:File[]=[]
 
   constructor(private testS:BarnaTestCuccosaService){
 
@@ -17,12 +20,66 @@ export class BTCCompComponent {
 
   postMakeAdmin(){
     this.testS.postMakeAdmin(this.username)
+    .subscribe({
+      next:(res)=>{
+        console.log(res)
+        return res
+      },
+      error:(err)=>{
+        console.log("Error in post make-admin ")
+        console.log(err)
+      }
+    })
   }
   postHello(){
     this.testS.postHello(this.ize)
+    .subscribe({
+      next:(res)=>{
+        this.postHelloRes=res
+        console.log(res)
+        return res
+      },
+      error:(err)=>{
+        this.postHelloRes=err
+        console.log("Error in post hello ")
+        console.log(err)
+      }
+    })
+  }
+  postfile() {
+    console.log(this.file[1].name)
+    const formData = new FormData();
+    for (let i = 0; i < this.file.length; i++) {
+      formData.append('images', this.file[i]);
+    }
+    
+    this.testS.postFile(formData).subscribe({
+      next: (res) => {
+        this.postHelloRes = res;
+        console.log(res);
+        return res;
+      },
+      error: (err) => {
+        this.postHelloRes = err;
+        console.log("Error in post hello ");
+        console.log(err);
+      }
+    });
   }
   getHello(){
     this.testS.getHello()
+    .subscribe({
+      next:(res)=>{
+        this.getHelloRes=res
+        console.log(res)
+        return res
+      },
+      error:(err)=>{
+        this.getHelloRes=err
+        console.log("Error in get ")
+        console.log(err)
+      }
+    })
   }
 
 
