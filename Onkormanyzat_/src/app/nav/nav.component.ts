4683b0b01305a693391ce9c9708473f4d3316ce9
Attila732 <kovacsattila478@gmail.com>
2024-03-04 10:38:33 +0100

    animations and design and bootstrap changes

--- Onkormanyzat_/src/app/nav/nav.component.ts
+++ Onkormanyzat_/src/app/nav/nav.component.ts
@@ -1,11 +1,26 @@
 import { Component, HostListener } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
-})
+  styleUrls: ['./nav.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        transform: 'translateY(-20px)'
+      })),
+      transition('void <=> *', animate('500ms ease-in-out')),
+    ]),
+  ],
+ 
+}
+)
+
+
+
 export class NavComponent {
+
   isNavbarOpen = false;
   isMobile = false;
 
@@ -20,3 +35,4 @@ export class NavComponent {
 
 
 }
+
