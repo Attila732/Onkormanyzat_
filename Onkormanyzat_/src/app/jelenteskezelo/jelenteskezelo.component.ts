4a8c17b45fdde67a26077d096f970f18a3c1d654
Attila732 <kovacsattila478@gmail.com>
2024-03-11 22:02:00 +0100

    tailwind design regform body

--- Onkormanyzat_/src/app/jelenteskezelo/jelenteskezelo.component.ts
+++ Onkormanyzat_/src/app/jelenteskezelo/jelenteskezelo.component.ts
@@ -1,47 +1,34 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { JelenteskezeloService } from '../jelenteskezelo.service';
 import { BejelentesAdatok } from '../models/BejelentesAdatok';
 import { BaseService } from '../base.service';
 import { ProfilAdatok } from '../models/ProfilAdatok';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-jelenteskezelo',
   templateUrl: './jelenteskezelo.component.html',
   styleUrls: ['./jelenteskezelo.component.css']
 })
-export class JelenteskezeloComponent implements OnDestroy{
+export class JelenteskezeloComponent {
   bejelentesModel = new BejelentesAdatok
-  private user: ProfilAdatok | null = null;
-  private subscription:Subscription|null=null
+  private user:ProfilAdatok|null=null;
 
-  constructor(private jelenteskezeloservice: JelenteskezeloService, private base: BaseService) {
-    this.getUserInfo()
-  }
-  getUserInfo(){
-    this.subscription = this.base.getUser().subscribe(
-      (res: any) => this.user = res
-    );
-  }
   inputForm() {
     if (this.user != null) {
-      this.bejelentesModel.userid = this.user.userId;
+      this.bejelentesModel.userid=this.user.userId;
       this.jelenteskezeloservice.postService(this.bejelentesModel);
     }
-    
-  }
-  
-  selectModel() {
-    
-  }
-  
-  
-  
-  ngOnDestroy(): void {
-    if (this.subscription != null) {
-      this.subscription.unsubscribe()
+
     }
 
+  selectModel(){
 
-  }
-};
+    }
+
+    constructor(private jelenteskezeloservice:JelenteskezeloService, private base:BaseService){
+    base.getUser().subscribe(
+      (res:any)=>this.user=res
+    );
+    }
+
+    };
