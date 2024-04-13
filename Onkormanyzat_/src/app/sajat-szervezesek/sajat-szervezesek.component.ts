1d9b79c30dce32a9741a09e6f266e81600cb4049
szaboszebasztian <szebasztianszabo01@gmail.com>
2024-04-13 16:16:08 +0200

    sajatszervezesek admin és orgAdmin ellenőrzésre vár

--- Onkormanyzat_/src/app/sajat-szervezesek/sajat-szervezesek.component.ts
+++ Onkormanyzat_/src/app/sajat-szervezesek/sajat-szervezesek.component.ts
@@ -3,7 +3,8 @@ import { SzervezesAdatok } from '../models/SzervezesAdatok';
 import { SzervezesService } from '../szervezes.service';
 import { ProfilAdatok } from '../models/ProfilAdatok';
 import { AuthService } from '../auth.service';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription, debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs';
+import { NgbTypeaheadSelectItemEvent } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-sajat-szervezesek',
@@ -14,6 +15,13 @@ export class SajatSzervezesekComponent {
   szervezesModel = new SzervezesAdatok()
   private user: ProfilAdatok | null = null;
   private subscription:Subscription[]= []
+  szervezesOrg: SzervezesAdatok[] = [];
+  orgs: { id: string; name: string }[] = [];
+  userRoles: any;
+  admin: boolean = false;
+  orgAdmin: boolean = false;
+  orgBooleanSzervezesek: boolean = false;
+  currentOrganization: {id: string, name: string}={id:"", name:""};
 
   szervezesek: SzervezesAdatok[] = [];
 
@@ -22,9 +30,26 @@ export class SajatSzervezesekComponent {
   }
 
   getUserInfo(){
-    this.subscription?.push(this.auth.getUser().subscribe(
-      (res: any) => this.user = res
-    ));
+    this.subscription.push(
+      this.auth.getUser().subscribe((res: any) => {
+        this.user = res;
+        this.subscription.push(
+          this.auth.getUserRoles().subscribe((roles: Map<String, boolean>) => {
+            this.userRoles = roles;
+
+            if (this.user != null && this.userRoles.get('ORG_ADMIN')) {
+              this.subscription.push(
+                this.auth.getOrgsForUser(this.user.userId, 0).subscribe({
+                  next: (res: any) => {
+                    this.orgs = res;
+                  },
+                })
+              );
+            }
+          })
+        );
+      })
+    );
   }
   inputForm() {
     if (this.user != null) {
@@ -65,5 +90,60 @@ export class SajatSzervezesekComponent {
     this.szervezesService.deleteSzervezes(termek.userId).subscribe(
       (res:any)=>{console.log("siker")}
     )
+  }
+
+  orgAdminKeres(){
+    this.orgAdmin = !this.orgAdmin
+  }
+
+  adminKeres(){
+    this.admin = !this.admin
+  }
+
+  orgRequest(){
+    if (this.currentOrganization != null) {
+      this.szervezesService.getSajatSzervezesekOrg(this.currentOrganization).subscribe(
+        (res:any)=>{this.szervezesOrg = res
+          this.orgBooleanSzervezesek = true;
+        }
+      )
+    }
+  }
+
+  updateOrgIdopont(termek:any){
+    this.szervezesService.updateSzervezesOrg(termek).subscribe(
+      (res:any)=>{console.log(res)}
+    );
+  }
+  
+  deleteOrgIdopont(termek:any){
+    this.szervezesService.deleteSzervezesOrg(termek.userId).subscribe(
+      (res:any)=>{console.log("siker")}
+    )
+  }
+
+  loadOrgsAdatok(value:string, pageNum: number, category: string){
+    return this.szervezesService.searchName( value, pageNum, category)
+  }
+
+  searchPeople = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      filter((searchTerm) => searchTerm.length >= 2),
+      switchMap((searchTerm) => this.loadOrgsAdatok("", 0, searchTerm))
+    );
+
+
+  resultFormatter = (result: {id: string, name: string}) => `${result.name}`;
+  inputFormatter = (result: {id: string, name: string}) => `${result.name}`;
+
+
+
+  onSelectItem(event: NgbTypeaheadSelectItemEvent<{id: string, name: string}>) {
+    event.preventDefault()
+    console.log(event.item.name)
+    this.currentOrganization=event.item;
+
   }
 }
--- Onkormanyzat_/src/app/szervezes.service.ts
+++ Onkormanyzat_/src/app/szervezes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { SzervezesAdatok } from './models/SzervezesAdatok';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ export class SzervezesService {
   szervezesadatok:SzervezesAdatok= new SzervezesAdatok();
 
   url = "resource/event/";
+  orgUrl = "resource/org/event";
 
   constructor(private http:HttpClient){
 
@@ -28,5 +30,35 @@ export class SzervezesService {
 
   getSajatSzervezesek(id: any) {
     return this.http.get(this.url + "sajat/" + id)
+  }
+
+  updateSzervezesOrg(body: any) {
+    return this.http.put(this.orgUrl + body.id, body)
+  }
+  
+  deleteSzervezesOrg(id: any) {
+    return this.http.delete(this.orgUrl + "del/" + id)
+  }
+  
+  getSajatSzervezesekOrg(id: any) {
+    return this.http.get(this.orgUrl + "sajat/" + id)
+  }
+  
+  searchName(value:string, pageNum:number, category:string){
+    const opt = {
+      params: new HttpParams()
+        .append("value", value)
+        .append("pageNum", pageNum)
+        .append("category", category)}
+    return this.http.get("/resource/org/search/name", opt).pipe(
+      map((res:any) => {
+        console.log('Successfully got org:', res);
+        return res;
+      }),
+      catchError((error) => {
+        console.error('Error in getorgByName:', error);
+        return throwError(() => error);
+      })
+    )
   }
 }
